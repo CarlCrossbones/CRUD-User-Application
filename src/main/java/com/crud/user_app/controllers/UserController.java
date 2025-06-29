5c435cf6d8447038878c168a305a402051a80163
George Wilkerson <george.wilkerson@okbu.edu>
2025-06-29 14:44:28 -0500

    first draft

--- src/main/java/com/crud/user_app/controllers/UserController.java
+++ src/main/java/com/crud/user_app/controllers/UserController.java
@@ -2,11 +2,15 @@ package com.crud.user_app.controllers;
 
 import com.crud.user_app.persistent.User;
 import com.crud.user_app.persistent.UserRepository;
+
+import jakarta.transaction.Transactional;
+
 import com.crud.user_app.controllers.UserSpecification;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -42,5 +46,26 @@ public class UserController {
         
         return userRepository.findAll(spec);
     }
-    
+
+
+    /**
+     * CREATE: An endpoint to facilitate the addition of new user's to the database
+     * @param name The name of the user
+     * @param age The age of the user
+     * @param birthState The birth state of the user
+     * @return A new user object
+     */
+    @PostMapping("/create")
+    @Transactional
+    public User addUser(String name, Integer age, String birthState) {
+        // Prevent duplicate entries along the "name" parameter.
+        if (!userRepository.findByName(name).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "User with name \""+name+"\" already exists");
+        }
+
+        User newUser = new User(name, age, birthState);
+
+        userRepository.save(newUser);
+        return newUser;
+    }
 }
